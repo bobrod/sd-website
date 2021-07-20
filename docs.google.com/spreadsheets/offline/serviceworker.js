9a3e26eb20a12399e19a9613aed15d97e80ea098
Robert Rodriguez <sw-erdnase@hotmail.com>
2021-07-21 07:09:33 +0800

    First commit

---
+++ docs.google.com/spreadsheets/offline/serviceworker.js
@@ -0,0 +1,18 @@
+'trix_2021.28-Tue_RC00-c1';
+self.document = self;
+self.window = self;
+_docs_flag_initialData={"kix-edrat":true,"docs-offline-hsu":"docs.google.com/sheets","docs-sw-eerf":true,"jobset":"prod","docs-offline-nnodi":100,"docs-localstore-dom":false,"docs-offline-lsuid":"u3d09edac7d132af6","ritzOfflineUrl":"/spreadsheets","docs-sw-ecfr":false,"docs-sw-eprfcf":true,"docs-esi":false,"docs-erd":true,"docs-offline-mrs":1073741824,"docs-eil":true,"udurls":true,"fatra":true,"docos-eos":true,"docs-sw-cache-prefix":"spreadsheets","docs-offline-ercidep":true,"kixOfflineUrl":"/document","docs-sw-nfhms":10,"docs-eac":false,"docs-localstore-iort":10000,"ncablmu":"https://docs.google.com/support/bin/static.py?page\u003dknown_issues.cs","docs-sw-eswcut":false,"docs-offline-oebp":"/offline/eventbusworker.js","docs-localstore-cide":true,"docs-sw-ccrdu":true,"docs-ecci":true,"docs-offline-uiaffd":true,"docs-sup":"/spreadsheets/u/0","dcau":"https://chrome.google.com/webstore/detail/ghbmnnjooekpmoecnnnilnnbdlolhkhi","punchOfflineUrl":"/presentation","docs-localstore-lral":500,"sgcn":"APISID","docs-offline-uoia":true,"docs-erkpp":false,"eooodab":false,"docs-ewtaoe":true,"lssv":7,"docs-obsImUrl":"https://ssl.gstatic.com/docs/common/netcheck.gif","docs-localstore-ilat":10000,"docs-sw-eddf":false,"drawingsOfflineUrl":"/drawings","docs-sw-enccr":false,"docs-cclt":57,"drive_url":"//drive.google.com/u/0?usp\u003dsheets_web","buildLabel":"trix_2021.28-Tue_RC00","docs-offline-dbfuac":20000,"docs-extension-id":"ghbmnnjooekpmoecnnnilnnbdlolhkhi","docs-sw-rpl":[]};
+_docs_swStartLoad=new Date().getTime();
+if (self.trustedTypes && self.trustedTypes.createPolicy) {
+var urls = {
+"\/static\/spreadsheets2\/client\/js\/2244502300-editor_sw_bin_editor_main.js" : 1,
+};
+var swPolicy = self.trustedTypes.createPolicy('docs#workerTemplate', {
+createScriptURL: function(url) {
+return urls.hasOwnProperty(url) ? url : "";
+}
+});
+importScripts(swPolicy.createScriptURL("\/static\/spreadsheets2\/client\/js\/2244502300-editor_sw_bin_editor_main.js"));
+} else {
+importScripts('\/static\/spreadsheets2\/client\/js\/2244502300-editor_sw_bin_editor_main.js');
+}
